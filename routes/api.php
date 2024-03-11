4389f1798e3f94865d364653c4114f62e4bd2609
Felicio Almeida <feliciorar@gmail.com>
2024-03-10 21:14:21 -0300

    added routes for tasks

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Rotas de autenticação de usuário
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
+
+// Rotas para tarefas protegidas (sem autenticação)
+// Route::middleware('auth.task')->group(function () {}
+Route::post('/tasks', [TaskController::class, 'create']);
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::put('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TaskController::class, 'delete']);
+
+
